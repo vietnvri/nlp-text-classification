f71b522179738ca2a7d1310475a5ad97e0b4820d
NguyenViet123 <viet241097@gmail.com>
2018-10-14 00:39:22 +0700

    vietnv

--- scrapweb.py
+++ scrapweb.py
@@ -24,6 +24,7 @@ def crawVnxpress(link):
 
     soup = BS(page.content, "html.parser")
     if soup.find('p', class_="Normal") == None:
+        print(link, "khong chua the Mormal")
         return ""
     return "".join([s.get_text() for s in soup.find_all('p', class_="Normal")]).replace("\n", " ")
 
@@ -35,6 +36,9 @@ def crawDantri(link):
         return ""
 
     soup = BS(page.content, "html.parser")
+    if soup.find(id="divNewsContent") == None:
+        print(link, "khong chua the divNewsContent")
+        return ""
     return "".join([s.get_text() for s in soup.find(id="divNewsContent").find_all("p")[:-1]]).replace("\n", " ")
 
 
@@ -45,28 +49,37 @@ def crawThanhnien(link):
         return ""
 
     soup = BS(page.content, "html.parser")
-    return "".join([s.get_text() for s in soup.find(id="abody").find_all('div')[:-1]]).replace("\n", " ")
+    if soup.find(id="abody") == None:
+        print(link, "khong chua the abody")
+        return ""
+    return "".join([s.get_text() for s in soup.find(id="abody").find_all('div')]).replace("\n", " ")
 
 
 # Rss cho cac labels
-thoi_su = ["https://vnexpress.net/rss/thoi-su.rss"]
-the_gioi = []
-kinh_doanh = []
-startup = []
-giai_tri = []
-the_thao = []
-phap_luat = []
-giao_duc = []
-truyen_cuoi = []
-suc_khoe = []
-doi_song = []
-tam_su = []
-khoa_hoc = []
-xe = []
-rss = {"thoi_su": thoi_su, "the_gioi": the_gioi, "kinh_doanh": kinh_doanh,
-       "startup": startup, "giai_tri": giai_tri, "tam_su": tam_su, "khoa_hoc": khoa_hoc,
+# thoi_su = ["https://vnexpress.net/rss/thoi-su.rss", "https://thanhnien.vn/rss/viet-nam.rss"]
+kinh_doanh = ["https://dantri.com.vn/kinh-doanh.rss", "https://thanhnien.vn/rss/kinh-doanh.rss",
+              "https://vnexpress.net/rss/kinh-doanh.rss", "https://vnexpress.net/rss/startup.rss"]
+the_gioi = ["https://dantri.com.vn/the-gioi.rss", "https://thanhnien.vn/rss/the-gioi.rss",
+            "https://vnexpress.net/rss/the-gioi.rss"]
+giai_tri = ["https://dantri.com.vn/giai-tri.rss", "https://thanhnien.vn/rss/ban-can-biet/giai-tri.rss",
+            "https://vnexpress.net/rss/giai-tri.rss"]
+the_thao = ["https://dantri.com.vn/the-thao.rss", "https://thethao.thanhnien.vn/rss/home.rss",
+            "https://vnexpress.net/rss/the-thao.rss"]
+phap_luat = ["https://dantri.com.vn/phap-luat.rss", "https://thanhnien.vn/rss/viet-nam/phap-luat.rss",
+             "https://vnexpress.net/rss/phap-luat.rss"]
+giao_duc = ["https://dantri.com.vn/giao-duc-khuyen-hoc.rss", "https://thanhnien.vn/rss/giao-duc.rss",
+            "https://vnexpress.net/rss/giao-duc.rss"]
+suc_khoe = ["https://dantri.com.vn/suc-khoe.rss", "https://thanhnien.vn/rss/doi-song/suc-khoe.rss",
+            "https://vnexpress.net/rss/suc-khoe.rss"]
+van_hoa = ["https://dantri.com.vn/van-hoa.rss", "https://thanhnien.vn/rss/van-hoa-nghe-thuat.rss"]
+khoa_hoc = ["https://dantri.com.vn/khoa-hoc-cong-nghe.rss", "https://vnexpress.net/rss/khoa-hoc.rss"]
+xe = ["https://dantri.com.vn/o-to-xe-may.rss", "https://xe.thanhnien.vn/rss/home.rss",
+      "https://vnexpress.net/rss/oto-xe-may.rss"]
+
+rss = {"the_gioi": the_gioi, "kinh_doanh": kinh_doanh,
+       "giai_tri": giai_tri, "khoa_hoc": khoa_hoc,
        "xe": xe, "the_thao": the_thao, "phap_luat": phap_luat, "giao_duc": giao_duc,
-       "truyen_cuoi": truyen_cuoi, "suc_khoe": suc_khoe, "doi_song": doi_song}
+       "suc_khoe": suc_khoe, "van_hoa": van_hoa}
 
 print("Number of classes: ", len(rss))
 
@@ -80,8 +93,13 @@ def getdatafrom(hostname, link):
         'vnexpress.net': crawVnxpress,
         "thanhnien.vn": crawThanhnien
     }
+    if "dantri.com.vn" in hostname:
+        return switcher["dantri.com.vn"](link)
+    elif "vnexpress.net" in hostname:
+        return switcher["vnexpress.net"](link)
+    else:
+        return switcher["thanhnien.vn"](link)
 
-    return switcher[hostname](link)
 
 def saveFile(content, path):
     # check file exist
