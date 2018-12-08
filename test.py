8e378566176b78eddd006362a773ccb61a5bf2ce
NguyenViet123 <viet241097@gmail.com>
2018-12-08 08:34:00 +0700

    vietnv

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-soup = BeautifulSoup(page.content, 'html.parser')
-
-print([type(item) for item in list(soup.children)])
